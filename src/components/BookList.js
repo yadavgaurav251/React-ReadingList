1e1b2e41a8da200de11519b72683627cf3174960
Gaurav Yadav <yadavgaurav251@gmail.com>
2020-06-18 16:00:06 +0530

    intial setup

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>Book1 </li>
+          <li>Book2</li>
+          <li>Book3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() { 
+        return (
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+          );
+    }
+}
+ 
+export default Navbar;
